e0a3114cd2ae9fdbc88b810989b2beac3a525c59
karaens <skaraen@gmail.com>
2020-01-25 04:47:53 +0530

    Add UNIQ in home menu

--- resources/views/20/countdown.blade.php
+++ resources/views/20/countdown.blade.php
@@ -8,17 +8,28 @@
 		<title>Currents' 20</title>
 		<!-- Fonts -->
 		<link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<link rel="stylesheet" type="text/css" media="screen" href="css/countdown.css" />
 	</head>
 	<body>
 			<div id='screen-container' class="fillParent">
         		<div class='header'>
-            		<div id='links-container' class="fillParent">
+            		<div class='links-container' class="fillParent">
 						<a href="events">Events</a>
 						<a href="eureka">Eureka</a>
 						<a href="volts">Volts</a>
 						<a href="workshops">Workshops</a>
 						<a href="cca">CCA</a>
+						<div class="dropdown">
+							<button class="dropbtn">UNIQ 
+							<i class="fa fa-caret-down"></i>
+							</button>
+							<div class="dropdown-content">
+								<a href="https://studentsbazaar.in/" target="_blank">Student Bazaar</a>
+								<a href="http://www.internshipinchennai.com/" target="_blank">Internship</a>
+								<a href="http://www.ieeefinalyearprojects.org/" target="_blank">IEEE Projects</a>
+							</div>
+						</div> 
 					</div>
         		</div>
         		<div class='middle'>
